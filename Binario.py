e6c3170d856af351b2caef277d1f6f1e4c89a382
JesusXimenez18 <152233466+JesusXimenez18@users.noreply.github.com>
2023-11-28 15:44:22 +0100

    Add files via upload

--- Binario.py
+++ Binario.py
@@ -1,15 +1,15 @@
 def esBinario(strbinario):
-    return all(bit == '0' or bit == '1' for bit in strbinario)
+    return all(digito == '0' or digito == '1' for digito in strbinario)
 
-def binario_a_decimal(strbinario):
+def BinADecimal(strbinario):
     if esBinario(strbinario):
         decimal = int(strbinario, 2)
         return decimal
     else:
         return None
 
-numero_binario = input("Introduce un número binario: ")
-resultado_decimal = binario_a_decimal(numero_binario)
+num_binario = input("Introduce un número binario: ")
+resultado_decimal = BinADecimal(num_binario)
 
 if resultado_decimal is not None:
     print(f"El número decimal equivalente es: {resultado_decimal}")
